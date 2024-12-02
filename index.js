7751991c30845ff0675e1f1423d6cbe8fc6e1064
tamarShapira <atifal2022@gmail.com>
2024-12-03 00:51:06 +0200

    my fourth commit message

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
+import express  from 'express';
+import cors from 'cors'
+//  const express =require('express');
+//  const cors =require( 'cors');
+const app=express();
+app.use(cors());
+const port=3000;
+app.listen(port,()=>{
+ console.log(`example app listening on https://localhost:${port}`);
+})
+
 import axios from 'axios';
+// const axios = require('axios');
 
 const options = {
-  method: 'GET',
-  url: 'https://api.render.com/v1/services?includePreviews=true&limit=20',
-  headers: {
-    accept: 'application/json',
-    authorization: 'Bearer rnd_7Oq8SXPviECk03Ftd5vtz3CuiT4n'
-  }
+ method: 'GET',
+ url: 'https://api.render.com/v1/services?limit=20',
+ headers: {
+   accept: 'application/json',
+   authorization: 'Bearer rnd_qrKDhzWln7p8TvItJKTvSWtKBWGS'
+ }
 };
+
+
+app.get('/', (req,res)=>{
+axios
+ .request(options)
+ .then(function (response) {
+   res.send(response.data);
+ })
+ .catch(function (error) {
+   res.send(error);
+ });
+})
